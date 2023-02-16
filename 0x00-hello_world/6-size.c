20d7c697332bf569ad3b48416c8a0378aaba219f
Eyad Almallah <eyad.almallah09@outlook.com>
2023-02-16 06:31:02 -0800

    q66

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,14 +15,12 @@ int main(void)
 
 	char a;
 	int b;
-	double c;
 	long int d;
 	long long e;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
-	printf("Size of an double: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(e));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
