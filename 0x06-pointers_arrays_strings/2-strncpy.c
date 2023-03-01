648fe91a84c19e0cd9f8c9918acac816e9417827
Eyad Almallah <eyad.almallah09@outlook.com>
2023-03-01 12:27:03 -0800

    q2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,38 @@
 #include "main.h"
 
 /**
- * _strncat - Write a function that concatenates two strings. it will use at
- *            most n bytes from src
+ * _strncpy - A function that copies a string.
  *
- * @dest: This is the output  dest
- * @src: This is the input src
- * @n: This is the number bytes fro src
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
  *
- * Return: This is my return
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int index, count;
+ * Return: @dest
+*/
 
-	for (index = 0; dest[index] != '\0'; index++)
-		;
-	for (count = 0; count < n && src[count] != '\0'; count++)
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	/**
+	 * if the length of source is less than n
+	 * write additional nullbytes to dest to
+	 * ensure that a total of n bytes is written
+	*/
+	while (i < n)
 	{
-		dest[index] = src[count];
-		index++;
+		dest[i] = '\0';
+		i++;
 	}
-	if (count < n)
-	{
-	dest[index] = '\0';
-	}
+
 	return (dest);
 }
