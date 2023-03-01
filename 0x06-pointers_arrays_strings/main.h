c154c7419f2b39b78eacb91aca3cc78742da20cf
Eyad Almallah <eyad.almallah09@outlook.com>
2023-03-01 12:50:52 -0800

    q111

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+/**
+ * Description: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory
+ */
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,6 +15,9 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *_strstr(char *haystack, char *needle);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
+
 #endif
